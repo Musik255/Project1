b43b9c4992fa93acbe6af3eb6aa34662ceb2a207
P ch <pkchvyrov@vk.com>
2023-11-08 17:14:39 +0300

    added imageView

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Павел Чвыров on 07.11.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
